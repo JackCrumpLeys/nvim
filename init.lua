b921c613031155421dcba39a3f9b16355c3b59bf
Jack crumpleys <jackcrumpleys@gmail.com>
2024-03-05 16:18:12 +1300

    python fix

--- init.lua
+++ init.lua
@@ -369,6 +369,7 @@ vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by 
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eys' })
+vim.keymap.set('n', 'gs', require('telescope.builtin').spell_suggest, { desc = '[S]pell' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
